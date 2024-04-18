1235645e52ae27b17c0f7e63e32168b02604c3fe
xhz13 <17420212201032@stu.xmu.edu.cn>
2024-04-18 13:36:46 +0800

    new file:   financerobot/.gitignore 	new file:   financerobot/.mvn/wrapper/maven-wrapper.jar 	new file:   financerobot/.mvn/wrapper/maven-wrapper.properties 	new file:   financerobot/mvnw 	new file:   financerobot/mvnw.cmd 	new file:   financerobot/pom.xml 	new file:   financerobot/src/main/java/com/finance/financerobot/FinancerobotApplication.java 	new file:   financerobot/src/main/resources/application.properties 	new file:   financerobot/src/test/java/com/finance/financerobot/FinancerobotApplicationTests.java

---
+++ financerobot/src/main/java/com/finance/financerobot/FinancerobotApplication.java
@@ -0,0 +1,13 @@
+package com.finance.financerobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancerobotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancerobotApplication.class, args);
+	}
+
+}
---
+++ financerobot/src/test/java/com/finance/financerobot/FinancerobotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finance.financerobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancerobotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
