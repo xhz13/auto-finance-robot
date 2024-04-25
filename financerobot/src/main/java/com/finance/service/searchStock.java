2a3ccc573e60a8c9c7de4737e516d0b7ba61a8b9
xhz13 <2331605944@qq.com>
2024-04-25 14:43:27 +0800

    modified:   financerobot/src/main/java/com/finance/service/searchStock.java

--- financerobot/src/main/java/com/finance/service/searchStock.java
+++ financerobot/src/main/java/com/finance/service/searchStock.java
@@ -1,5 +1,10 @@
 package com.finance.service;
 
+import org.springframework.web.bind.annotation.PostMapping;
+
 public class searchStock {
-    
+    @PostMapping("/searchStock")
+    public String searchStock(String stockName) {
+        return "Stock Name: " + stockName;
+    }
 }
