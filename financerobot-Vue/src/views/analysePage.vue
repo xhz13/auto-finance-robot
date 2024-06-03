29caccefd41447988e7673ca0c745dd73cc382c6
xhz13 <2331605944@qq.com>
2024-06-03 16:44:32 +0800

    modified:   financerobot-Vue/src/views/analysePage.vue

--- financerobot-Vue/src/views/analysePage.vue
+++ financerobot-Vue/src/views/analysePage.vue
@@ -164,6 +164,194 @@ response.data.forEach((item: any) => {
         totProf: item.totProf,
         totShareEquit: item.totShareEquit,
       }));
+
+    /*
+    *新的表
+    */
+
+      var app: any = {};
+      type EChartsOption = echarts.EChartsOption;
+      
+      var chartDom = document.getElementById('chart2')!;
+      var myChart2 = echarts.init(chartDom);
+      var option: EChartsOption;
+      
+      const posList = [
+        'left',
+        'right',
+        'top',
+        'bottom',
+        'inside',
+        'insideTop',
+        'insideLeft',
+        'insideRight',
+        'insideBottom',
+        'insideTopLeft',
+        'insideTopRight',
+        'insideBottomLeft',
+        'insideBottomRight'
+      ] as const;
+      
+      app.configParameters = {
+        rotate: {
+          min: -90,
+          max: 90
+        },
+        align: {
+          options: {
+            left: 'left',
+            center: 'center',
+            right: 'right'
+          }
+        },
+        verticalAlign: {
+          options: {
+            top: 'top',
+            middle: 'middle',
+            bottom: 'bottom'
+          }
+        },
+        position: {
+          options: posList.reduce(function (map, pos) {
+            map[pos] = pos;
+            return map;
+          }, {} as Record<string, string>)
+        },
+        distance: {
+          min: 0,
+          max: 100
+        }
+      };
+      
+      app.config = {
+        rotate: 90,
+        align: 'left',
+        verticalAlign: 'middle',
+        position: 'insideBottom',
+        distance: 15,
+        onChange: function () {
+          const labelOption: BarLabelOption = {
+            rotate: app.config.rotate as BarLabelOption['rotate'],
+            align: app.config.align as BarLabelOption['align'],
+            verticalAlign: app.config
+              .verticalAlign as BarLabelOption['verticalAlign'],
+            position: app.config.position as BarLabelOption['position'],
+            distance: app.config.distance as BarLabelOption['distance']
+          };
+          myChart2.setOption<echarts.EChartsOption>({
+            series: [
+              {
+                label: labelOption
+              },
+              {
+                label: labelOption
+              },
+              {
+                label: labelOption
+              },
+              {
+                label: labelOption
+              }
+            ]
+          });
+        }
+      };
+      
+      type BarLabelOption = NonNullable<echarts.BarSeriesOption['label']>;
+      
+      const labelOption: BarLabelOption = {
+        show: true,
+        position: app.config.position as BarLabelOption['position'],
+        distance: app.config.distance as BarLabelOption['distance'],
+        align: app.config.align as BarLabelOption['align'],
+        verticalAlign: app.config.verticalAlign as BarLabelOption['verticalAlign'],
+        rotate: app.config.rotate as BarLabelOption['rotate'],
+        formatter: '{c}  {name|{a}}',
+        fontSize: 16,
+        rich: {
+          name: {}
+        }
+      };
+      
+      option = {
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'shadow'
+          }
+        },
+        legend: {
+          data: ['Forest', 'Steppe', 'Desert', 'Wetland']
+        },
+        toolbox: {
+          show: true,
+          orient: 'vertical',
+          left: 'right',
+          top: 'center',
+          feature: {
+            mark: { show: true },
+            dataView: { show: true, readOnly: false },
+            magicType: { show: true, type: ['line', 'bar', 'stack'] },
+            restore: { show: true },
+            saveAsImage: { show: true }
+          }
+        },
+        xAxis: [
+          {
+            type: 'category',
+            axisTick: { show: false },
+            data: ['2012', '2013', '2014', '2015', '2016']
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ],
+        series: [
+          {
+            name: 'Forest',
+            type: 'bar',
+            barGap: 0,
+            label: labelOption,
+            emphasis: {
+              focus: 'series'
+            },
+            data: [320, 332, 301, 334, 390]
+          },
+          {
+            name: 'Steppe',
+            type: 'bar',
+            label: labelOption,
+            emphasis: {
+              focus: 'series'
+            },
+            data: [220, 182, 191, 234, 290]
+          },
+          {
+            name: 'Desert',
+            type: 'bar',
+            label: labelOption,
+            emphasis: {
+              focus: 'series'
+            },
+            data: [150, 232, 201, 154, 190]
+          },
+          {
+            name: 'Wetland',
+            type: 'bar',
+            label: labelOption,
+            emphasis: {
+              focus: 'series'
+            },
+            data: [98, 77, 101, 99, 40]
+          }
+        ]
+      };
+      
+      option && myChart2.setOption(option);
+
+
 });
 
 
@@ -173,6 +361,16 @@ response.data.forEach((item: any) => {
   <div>
   <searchBar/>
   <div id="main" style="width:1000px;height:400px"></div>
+  <!-- TODO:居民经济表格表格 -->
+  <div id="chart2" style="width:1000px;height:400px"></div>
+
+  <!-- TODO:矿业数据表格 -->
+
+  <!-- TODO:资产结构、负债结构、流动负债趋势、营业收入和净利润趋势、营业成本趋势、经营现金流趋势、现金流结构、e皮s趋势、roa、roe趋势，分析师估值数据折现 -->
+
+  <!-- TODO:投入产出基本流量表 -->
+
+   
   <h1>企业情况</h1>
   <el-table :data="tableData" style="width: 100%">
     <el-table-column prop="year" label="年份" />
